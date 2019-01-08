7b3258d57c58dca2d4e61d0f29590067e94b9eec
Grupa 2 Java <alexandra.dumitru1218@gmail.com>
2019-01-08 21:02:27 +0200

    Initial commit

---
+++ src/main/java/Savarina.java
@@ -0,0 +1,16 @@
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+@Data
+
+public class Savarina {
+
+    private String culoare;
+    private String taste;
+    }
+
+
